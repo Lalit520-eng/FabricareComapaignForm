11e824065c3e70a97573f8ecc371c97245b86e93
lalit-PC <lalit-PC@DESKTOP-TDJ0426>
2023-09-13 11:39:31 +0530

    Initial Push

---
+++ ImportFabricareCompaignData.aspx
@@ -0,0 +1,93 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ImportFabricareCompaignData.aspx.cs" Inherits="FabricareComapaignForm.ImportFabcareCompaignData" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Import Fabricare Campaign Form</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+           
+        }
+     .form-title {
+    text-align: center;
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 20px;
+    color: #ffffff;
+    background-color: #333333;
+    padding: 10px;
+    border-radius: 4px;
+    width:50%;
+    align-items: center;
+    justify-content: center;
+  }
+        .container {
+            max-width: 650px;
+            margin: 50px auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+        .header {
+            text-align: center;
+            font-size: 24px;
+            margin-bottom: 20px;
+        }
+
+        .form-group {
+            margin-bottom: 20px;
+        }
+
+        label {
+            display: block;
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+
+        input[type="file"] {
+            display: block;
+            width: 95%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            background-color: #fff;
+            font-size: 16px;
+        }
+
+        .btn-import {
+            
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 3px;
+            cursor: pointer;
+            font-size: 16px;
+            background-color: #333333;     
+        }
+
+        .btn-import:hover {
+            background-color: #0056b3;
+        }
+    </style>
+</head>
+<body>
+
+ <div class="container">
+       <center> <div class="form-title">Import Fabricare Campaign Data</div></center>
+        <form id="form1" runat="server">
+            <div class="form-group">   
+            <label for="fileUpload">Select CSV File:</label>
+            <asp:FileUpload ID="fileUpload" runat="server" />
+             <asp:Label ID="lblMessage" runat="server"></asp:Label>
+            </div>
+             <center><asp:Button ID="btnImport" class="btn-import" runat="server" Text="Import File" OnClick="btnImport_Click" /></center>
+        </form>
+    </div>
+</body>
+</html>
---
+++ ImportFabricareCompaignData.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Configuration;
+
+namespace FabricareComapaignForm
+{
+    public partial class ImportFabcareCompaignData : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        protected void btnImport_Click(object sender, EventArgs e)
+        {
+            if (fileUpload.HasFile && Path.GetExtension(fileUpload.FileName).Equals(".csv", StringComparison.OrdinalIgnoreCase))
+            {
+                try
+                {
+                    //string connectionString = "your_connection_string_here";
+                    string connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        using (StreamReader reader = new StreamReader(fileUpload.PostedFile.InputStream))
+                        {
+                            bool isFirstRow = true;
+                            while (!reader.EndOfStream)
+                            {
+                                string line = reader.ReadLine();
+                                if (isFirstRow)
+                                {
+                                    isFirstRow = false;
+                                    continue; // Skip the header row
+                                }
+   
+                                string[] values = line.Split(',');
+
+                                string query = "INSERT INTO Tbl_SFMS_CampaignDump (Mobile_Number,Email_Id,Coupon_Code,Updated_Date) VALUES (@Mobile_Number,@Email_Id, @Coupon_Code,getdate())";
+                                using (SqlCommand command = new SqlCommand(query, connection))
+                                {
+                                    command.Parameters.AddWithValue("@Mobile_Number", values[0]);
+                                    command.Parameters.AddWithValue("@Email_Id", values[1]);
+                                    command.Parameters.AddWithValue("@Coupon_Code", values[2]);
+                                    command.ExecuteNonQuery();
+                                }
+                            }
+                        }
+                    }
+                    // Display success message
+                    ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('CSV data imported successfully.');", true);
+                }
+                catch (Exception ex)
+                {
+                    // Display error message
+                    ClientScript.RegisterStartupScript(this.GetType(), "alert", $"alert('Error: {ex.Message}');", true);
+                }
+            }
+            else
+            {
+                // Display file format error message
+                ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Please select a valid CSV file.');", true);
+            }
+        }
+    }
+}
+    
